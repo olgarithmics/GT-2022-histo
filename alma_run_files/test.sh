93427a66decc88ce597f284d622ea9a615801ee7
admin_ofourkioti <admin_ofourkioti@30357CMBDTL.icr.ac.uk>
2023-08-15 16:13:29 +0100

    train fold 1

--- alma_run_files/test.sh
+++ alma_run_files/test.sh
@@ -4,8 +4,8 @@
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=10
 #SBATCH --time=100:00:00
-#SBATCH --output=/home/ofourkioti/Projects/tmi2022/results/test_cam16.out
-#SBATCH --error=/home/ofourkioti/Projects/tmi2022/results/test_cam16.err
+#SBATCH --output=/home/ofourkioti/Projects/tmi2022/results/test_tcga.out
+#SBATCH --error=/home/ofourkioti/Projects/tmi2022/results/test_tcga.err
 #SBATCH --gres=gpu:1
 #SBATCH --partition=gpuhm
 
@@ -13,20 +13,20 @@ module use /opt/software/easybuild/modules/all/
 module load Mamba
 source ~/.bashrc
 mamba activate  dl_torch
-cd /home/ofourkioti/Projects/tmi2022/
+cd /home/ofourkioti/Projects/GT-2022-histo/
 
 for i in {0..4};
 do export CUDA_VISIBLE_DEVICES=0
 python main.py \
 --n_class 2 \
---data_path '/data/scratch/DBI/DUDBI/DYNCESYS/OlgaF/tmi/cam-16' \
---val_set "cam_16_splits/test_${i}.txt" \
+--data_path '/data/scratch/DBI/DUDBI/DYNCESYS/OlgaF/tmi/tcga_lung/' \
+--val_set "TCGA_LUNG_splits/test_${i}.txt" \
 --model_path "graph_transformer/saved_models/" \
 --log_path "graph_transformer/runs/" \
---task_name "train_cam16_tile_features_${i}"  \
+--task_name "tcga_lung_${i}"  \
 --batch_size 1 \
 --test \
 --log_interval_local 5 \
---resume "graph_transformer/saved_models/train_cam16_tile_features_${i}..pth"
+--resume "graph_transformer/saved_models/tcga_lung_${i}.pth"
 
 done
