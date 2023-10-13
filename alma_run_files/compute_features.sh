f31f52f1db101f1d6383a34f146657fe490f5534
admin_ofourkioti <admin_ofourkioti@30357CMBDTL.icr.ac.uk>
2023-10-13 16:18:01 +0100

    compute graphs for rcc data

--- alma_run_files/compute_features.sh
+++ alma_run_files/compute_features.sh
@@ -21,9 +21,9 @@ cd /home/ofourkioti/Projects/GT-2022-histo/feature_extractor/
 #python compute_feats_gtp.py --weights "DSMIL_extractors/20x/model-v0.pth" --dataset "/data/scratch/DBI/DUDBI/DYNCESYS/OlgaF/tmi/Colonoscopy/colon_patches/patches/*" --output "/data/scratch/DBI/DUDBI/DYNCESYS/OlgaF/tmi/Colonoscopy/graphs/" --slide_dir /data/scratch/DBI/DUDBI/DYNCESYS/OlgaF/tmi/Colonoscopy/segment_dataset/
 
 #python compute_feats_gtp.py --weights "DSMIL_extractors/20x/model-v0.pth" --dataset "/data/scratch/DBI/DUDBI/DYNCESYS/OlgaF/slides/camelyon17/patches/*" --output "/data/scratch/DBI/DUDBI/DYNCESYS/OlgaF/tmi/cam-17/gtp_features/" --slide_dir /data/scratch/DBI/DUDBI/DYNCESYS/OlgaF/slides/camelyon17/images/
-python compute_feats_gtp.py --weights "DSMIL_extractors/tcga_lung/model-v0.pth" --dataset "/data/scratch/DBI/DUDBI/DYNCESYS/OlgaF/tmi/rcc/patches/*" --output "/data/scratch/DBI/DUDBI/DYNCESYS/OlgaF/tmi/rcc/rcc_graphs/" --slide_dir /data/scratch/DBI/DUDBI/DYNCESYS/OlgaF/slides/TCGA_RCC/
+#python compute_feats_gtp.py --weights "DSMIL_extractors/tcga_lung/model-v0.pth" --dataset "/data/scratch/DBI/DUDBI/DYNCESYS/OlgaF/tmi/rcc/patches/*" --output "/data/scratch/DBI/DUDBI/DYNCESYS/OlgaF/tmi/rcc/rcc_graphs/" --slide_dir /data/scratch/DBI/DUDBI/DYNCESYS/OlgaF/slides/TCGA_RCC/
 #python compute_feats_res.py --weights "DSMIL_extractors/camelyon/model-v0.pth" --dataset "/data/scratch/DBI/DUDBI/DYNCESYS/OlgaF/tmi/cam-16/patches/*" --output "/data/scratch/DBI/DUDBI/DYNCESYS/OlgaF/tmi/sad_mil/feats/" --slide_dir /data/scratch/DBI/DUDBI/DYNCESYS/OlgaF/slides/camelyon16/
-#python compute_feats_res.py --weights "DSMIL_extractors/tcga_lung/model-v0.pth" --dataset "/data/scratch/DBI/DUDBI/DYNCESYS/OlgaF/tmi/rcc/patches/*" --output "/data/scratch/DBI/DUDBI/DYNCESYS/OlgaF/tmi/rcc/rcc_features/" --slide_dir /data/scratch/DBI/DUDBI/DYNCESYS/OlgaF/slides/TCGA_RCC/
+python compute_feats_res.py --weights "DSMIL_extractors/tcga_lung/model-v0.pth" --dataset "/data/scratch/DBI/DUDBI/DYNCESYS/OlgaF/tmi/rcc/patches/*" --output "/data/scratch/DBI/DUDBI/DYNCESYS/OlgaF/tmi/rcc/rcc_features/" --slide_dir /data/scratch/DBI/DUDBI/DYNCESYS/OlgaF/slides/TCGA_RCC/
 
 
 
