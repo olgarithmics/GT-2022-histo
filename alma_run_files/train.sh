a0f452173b60eaecd691e40c6dcbb7544a9546bf
admin_ofourkioti <admin_ofourkioti@30357CMBDTL.icr.ac.uk>
2023-10-16 15:13:54 +0100

    alma

--- alma_run_files/train.sh
+++ alma_run_files/train.sh
@@ -4,8 +4,8 @@
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=10
 #SBATCH --time=100:00:00
-#SBATCH --output=/home/ofourkioti/Projects/GT-2022-histo/results/train_tcga.out
-#SBATCH --error=/home/ofourkioti/Projects/GT-2022-histo/results/train_tcga.err
+#SBATCH --output=/home/ofourkioti/Projects/GT-2022-histo/results/train_rcc.out
+#SBATCH --error=/home/ofourkioti/Projects/GT-2022-histo/results/train.err
 #SBATCH --gres=gpu:1
 #SBATCH --partition=gpuhm
 
@@ -15,20 +15,20 @@ source ~/.bashrc
 mamba activate  dl_torch
 cd /home/ofourkioti/Projects/GT-2022-histo/
 
-CUDA_VISIBLE_DEVICES=0 python main.py --n_class 2 --data_path "/data/scratch/DBI/DUDBI/DYNCESYS/OlgaF/tmi/tcga_lung/" \
---train_set "TCGA_LUNG_splits/train_3.txt" --val_set "TCGA_LUNG_splits/val_3.txt" --model_path "graph_transformer/saved_models/" \
---log_path "graph_transformer/runs/" \
---task_name "tcga_lung_3" \
---batch_size 4 \
---train \
---log_interval_local 5
-
-#for i in {0..3};
-#do CUDA_VISIBLE_DEVICES=0 python main.py --n_class 2 --data_path "/data/scratch/DBI/DUDBI/DYNCESYS/OlgaF/tmi/tcga_lung/" \
-#--train_set "TCGA_LUNG_splits/train_${i}.txt" --val_set "TCGA_LUNG_splits/val_${i}.txt" --model_path "graph_transformer/saved_models/" \
+#CUDA_VISIBLE_DEVICES=0 python main_2.py --n_class 2 --data_path "/data/scratch/DBI/DUDBI/DYNCESYS/OlgaF/tmi/tcga_lung/" \
+#--train_set "TCGA_LUNG_splits/train_3.txt" --val_set "TCGA_LUNG_splits/val_3.txt" --model_path "graph_transformer/saved_models/" \
 #--log_path "graph_transformer/runs/" \
-#--task_name "tcga_lung_${i}" \
+#--task_name "tcga_lung_3" \
 #--batch_size 4 \
 #--train \
 #--log_interval_local 5
-#done
+
+for i in {0..4};
+do CUDA_VISIBLE_DEVICES=0 python main.py --n_class 3 --data_path "/data/scratch/DBI/DUDBI/DYNCESYS/OlgaF/tmi/rcc/" \
+--train_set "rcc_splits/train_${i}.txt" --val_set "rcc_splits/val_${i}.txt" --model_path "graph_transformer/saved_models/" \
+--log_path "graph_transformer/runs/" \
+--task_name "rcc_${i}" \
+--batch_size 4 \
+--train \
+--log_interval_local 5
+done
