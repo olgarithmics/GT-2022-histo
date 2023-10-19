2ebe05e3b015f2641c11495ed4a17aad18c91936
admin_ofourkioti <admin_ofourkioti@30357CMBDTL.icr.ac.uk>
2023-10-19 13:14:56 +0100

    alma

--- alma_run_files/test.sh
+++ alma_run_files/test.sh
@@ -4,7 +4,7 @@
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=10
 #SBATCH --time=100:00:00
-#SBATCH --output=/home/ofourkioti/Projects/GT-2022-histo/results/test_tcga.out
+#SBATCH --output=/home/ofourkioti/Projects/GT-2022-histo/results/test_colon.out
 #SBATCH --error=/home/ofourkioti/Projects/GT-2022-histo/results/test_tcga.err
 #SBATCH --gres=gpu:1
 #SBATCH --partition=gpuhm
@@ -15,18 +15,18 @@ source ~/.bashrc
 mamba activate  dl_torch
 cd /home/ofourkioti/Projects/GT-2022-histo/
 
-for i in {0..3};
+for i in {0..4};
 do export CUDA_VISIBLE_DEVICES=0
 python main.py \
 --n_class 2 \
---data_path '/data/scratch/DBI/DUDBI/DYNCESYS/OlgaF/tmi/tcga_lung/' \
---val_set "tcga_lung_files/test_${i}.txt" \
+--data_path '/data/scratch/DBI/DUDBI/DYNCESYS/OlgaF/tmi/PDAC_TMA/graphs' \
+--val_set "pdac_splits/test_${i}.txt" \
 --model_path "graph_transformer/saved_models/" \
 --log_path "graph_transformer/runs/" \
---task_name "tcga_lung_${i}"  \
+--task_name "pdac_${i}"  \
 --batch_size 1 \
 --test \
 --log_interval_local 5 \
---resume "graph_transformer/saved_models/tcga_lung_${i}.pth"
+--resume "graph_transformer/saved_models/pdac_${i}.pth"
 
 done
