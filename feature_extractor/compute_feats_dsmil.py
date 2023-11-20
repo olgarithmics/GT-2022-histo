37cb3b5f8880d26f2f34a74491a88c3e6b6aa5df
admin_ofourkioti <admin_ofourkioti@30357CMBDTL.icr.ac.uk>
2023-11-20 23:59:49 +0000

    compute feats

--- feature_extractor/compute_feats_dsmil.py
+++ feature_extractor/compute_feats_dsmil.py
@@ -135,7 +135,7 @@ def compute_tree_feats(args, low_patches, embedder_low, embedder_high, data_slid
                                         feats = feats.cpu().numpy() + 0.25 * feats_list[count]
                             elif args.tree_fusion == 'cat':
                                         expanded_feats_list = np.expand_dims(feats_list[count], axis=0)
-                                        print (expanded_feats_list.shape, feats.shape)
+
                                         feats = np.concatenate((feats.cpu().numpy(), expanded_feats_list), axis=0)
                             else:
                                         raise NotImplementedError(
