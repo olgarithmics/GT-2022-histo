1cc480501d75c4a88b97d526954b2603493525c8
admin_ofourkioti <admin_ofourkioti@30357CMBDTL.icr.ac.uk>
2023-11-22 18:53:12 +0000

    compute feats

--- feature_extractor/compute_feats_dsmil.py
+++ feature_extractor/compute_feats_dsmil.py
@@ -136,7 +136,6 @@ def compute_tree_feats(args, low_patches, embedder_low, embedder_high, data_slid
                                     pass
                                 else:
                                     high_patch = high_patch.to(device, non_blocking=True)
-                                    print (high_patch.shape)
                                     feats, classes = embedder_high(high_patch)
                                     if args.tree_fusion == 'fusion':
                                                 feats = feats.cpu().numpy() + 0.25 * feats_list[patch_count]
