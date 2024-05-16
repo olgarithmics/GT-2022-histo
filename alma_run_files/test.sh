7a9afddf073052114582e010d313015b99b6f40d
admin_ofourkioti <admin_ofourkioti@30357CMBDTL.icr.ac.uk>
2024-05-16 10:26:08 +0100

    alma test

--- alma_run_files/test.sh
+++ alma_run_files/test.sh
@@ -4,7 +4,7 @@
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=10
 #SBATCH --time=100:00:00
-#SBATCH --output=/home/ofourkioti/Projects/GT-2022-histo/results/test_colon.out
+#SBATCH --output=/home/ofourkioti/Projects/GT-2022-histo/results/test_lung.out
 #SBATCH --error=/home/ofourkioti/Projects/GT-2022-histo/results/error.err
 #SBATCH --gres=gpu:1
 #SBATCH --partition=gpuhm
@@ -19,14 +19,14 @@ for i in {0..5};
 do export CUDA_VISIBLE_DEVICES=0
 python main.py \
 --n_class 2 \
---data_path "/data/scratch/DBI/DUDBI/DYNCESYS/OlgaF/tmi/Colonoscopy/tmi_feats/" \
---val_set "colon_splits/test_${i}.txt" \
+--data_path"/data/scratch/DBI/DUDBI/DYNCESYS/OlgaF/tmi/lung/"  \
+--val_set "/home/ofourkioti/Projects/splits/data_files/splits/lung_splits/test_${i}.txt" \
 --model_path "graph_transformer/saved_models/" \
 --log_path "graph_transformer/runs/" \
---task_name "colon_${i}"  \
+--task_name "gtp_lung_${i}"  \
 --batch_size 1 \
 --test \
 --log_interval_local 5 \
---resume "graph_transformer/saved_models/colon_${i}.pth"
+--resume "graph_transformer/saved_models/gtp_lung_${i}.pth"
 
 done
