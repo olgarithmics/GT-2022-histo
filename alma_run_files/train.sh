8819e907e2d6c137011e9f47cb705a8ab22af6ac
admin_ofourkioti <admin_ofourkioti@30357CMBDTL.icr.ac.uk>
2024-05-21 23:45:34 +0100

    rcc

--- alma_run_files/train.sh
+++ alma_run_files/train.sh
@@ -23,13 +23,26 @@ cd /home/ofourkioti/Projects/GT-2022-histo/
 #--train \
 #--log_interval_local 5
 
+#for i in {0..4};
+#do CUDA_VISIBLE_DEVICES=0 python main.py --n_class 2 --data_path "/data/scratch/DBI/DUDBI/DYNCESYS/OlgaF/tmi/brca/"\
+#--train_set "/home/ofourkioti/Projects/HistoTree/brca_files/train_${i}.txt"\
+#--val_set "/home/ofourkioti/Projects/HistoTree/brca_files/val_${i}.txt"\
+#--model_path "graph_transformer/saved_models/" \
+#--log_path "graph_transformer/runs/" \
+#--task_name "gtp_brca_${i}"\
+#--batch_size 4 \
+#--train \
+#--log_interval_local 5
+#done
+
+
 for i in {0..4};
-do CUDA_VISIBLE_DEVICES=0 python main.py --n_class 2 --data_path "/data/scratch/DBI/DUDBI/DYNCESYS/OlgaF/tmi/brca/" \
---train_set "/home/ofourkioti/Projects/HistoTree/brca_files/train_${i}.txt" \
---val_set "/home/ofourkioti/Projects/HistoTree/brca_files/val_${i}.txt" \
+do CUDA_VISIBLE_DEVICES=0 python main.py --n_class 3 --data_path "/data/scratch/DBI/DUDBI/DYNCESYS/OlgaF/tmi/rcc/"\
+--train_set "/home/ofourkioti/Projects/HistoTree/rcc_files/train_${i}.txt"\
+--val_set "/home/ofourkioti/Projects/HistoTree/rcc_files/val_${i}.txt"\
 --model_path "graph_transformer/saved_models/" \
 --log_path "graph_transformer/runs/" \
---task_name "gtp_brca_${i}" \
+--task_name "gtp_rcc_${i}"\
 --batch_size 4 \
 --train \
 --log_interval_local 5
