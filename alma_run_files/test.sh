ae2eee7df1df1446e789e197fc1cea5991998ac2
admin_ofourkioti <admin_ofourkioti@30357CMBDTL.icr.ac.uk>
2024-05-22 18:31:22 +0100

    alma

--- alma_run_files/test.sh
+++ alma_run_files/test.sh
@@ -17,5 +17,5 @@ cd /home/ofourkioti/Projects/GT-2022-histo/
 
 
 for i in {0..4};
-do CUDA_VISIBLE_DEVICES=0 python main.py --n_class 2 --data_path "/data/scratch/DBI/DUDBI/DYNCESYS/OlgaF/tmi/rcc/"  --val_set "/home/ofourkioti/Projects/HistoTree/rcc_files/test_${i}.txt" --model_path "graph_transformer/saved_models/" --log_path "graph_transformer/runs/" --task_name "gtp_rcc_${i}" --batch_size 1 --test --log_interval_local 5  --resume "graph_transformer/saved_models/gtp_rcc_${i}.pth"
+do CUDA_VISIBLE_DEVICES=0 python main.py --n_class 3 --data_path "/data/scratch/DBI/DUDBI/DYNCESYS/OlgaF/tmi/rcc/"  --val_set "/home/ofourkioti/Projects/HistoTree/rcc_files/test_${i}.txt" --model_path "graph_transformer/saved_models/" --log_path "graph_transformer/runs/" --task_name "gtp_rcc_${i}" --batch_size 1 --test --log_interval_local 5  --resume "graph_transformer/saved_models/gtp_rcc_${i}.pth"
 done
